70c941067e4b3aa413c20a55051bfebd7f57ec4d
SirKrilhos <leoft97@gmail.com>
2016-10-02 12:36:59 -0300

    trocotest

--- Source Code Inspection/test/TrocoTest.java
+++ Source Code Inspection/test/TrocoTest.java
@@ -68,7 +68,7 @@ public class TrocoTest {
     
     
             Assert.assertEquals(expResult, result);
-            // TODO review the generated test code and remove the default call to fail.
+            
             fail("The test case is a prototype.");
         }
     
